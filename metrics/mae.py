57293265036341decf4ea531393d81321d68d244
Jaeyunlim <97609649+JaeyunLim@users.noreply.github.com>
2026-06-03 22:33:51 +0900

    Update mae.py

--- metrics/mae.py
+++ metrics/mae.py
@@ -1,1 +1,10 @@
 # TODO. implement mae score
+import numpy as np
+
+
+def mae(y_true, y_pred):
+    y_true = np.asarray(y_true, dtype=np.float64).ravel()
+    y_pred = np.asarray(y_pred, dtype=np.float64).ravel()
+
+
+    return np.mean(np.abs(y_true - y_pred))
