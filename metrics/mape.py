6650df182fcd5d1ee9a75b17bddd89fcc920a892
Jaeyunlim <97609649+JaeyunLim@users.noreply.github.com>
2026-06-03 22:39:47 +0900

    Updating  MAPE formula

--- metrics/mape.py
+++ metrics/mape.py
@@ -1,1 +1,10 @@
 # TODO. implement mape score
+import numpy as np
+
+
+def mape(y_true, y_pred):
+    
+    y_true = np.asarray(y_true)
+    y_pred = np.asarray(y_pred)
+
+    return np.mean(np.abs((y_true - y_pred) / y_true)) * 100
